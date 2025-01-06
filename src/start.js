792950e6c5977b0ac60d77421ad016b9a25e1e55
Dimdim28 <wwwdimanes1@gmail.com>
2025-01-06 12:05:02 +0200

    Test all nodes connected and sending files at the same time

--- src/start.js
+++ src/start.js
@@ -4,13 +4,44 @@ const Network = require("./network");
   console.log("CA certificate and keys already exist.");
 
   const network = new Network({
-    nodes: ["Node1", "Node2"],
-    connections: [
-      { name: "Node1", peers: ["Node2"] },
-      // { name: "Node2", peers: ["Node3"] },
-      // { name: "Node3", peers: ["Node1"] },
+    nodes: ["Node1", "Node2", "Node3"],
+    // connections: [
+    //   { name: "Node1", peers: ["Node2"] },
+    //   { name: "Node2", peers: ["Node3"] },
+    //   { name: "Node3", peers: ["Node1"] },
+    // ],
+    files: [
+      {
+        sender: "Node1",
+        receiver: "Node2",
+        filePath: "./textFromNode1to2.txt",
+      },
+      {
+        sender: "Node1",
+        receiver: "Node3",
+        filePath: "./textFromNode1to3.txt",
+      },
+      {
+        sender: "Node2",
+        receiver: "Node1",
+        filePath: "./textFromNode2to1.txt",
+      },
+      {
+        sender: "Node2",
+        receiver: "Node3",
+        filePath: "./textFromNode2to3.txt",
+      },
+      {
+        sender: "Node3",
+        receiver: "Node1",
+        filePath: "./textFromNode3to1.txt",
+      },
+      {
+        sender: "Node3",
+        receiver: "Node2",
+        filePath: "./textFromNode3to2.txt",
+      },
     ],
-    files: [{ sender: "Node1", receiver: "Node2", filePath: "./text.txt" }],
   });
 
   await network.initializeNodes();
